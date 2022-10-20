2ba29065da3320705e4222003c6724207ff59356
Tuan Tang <tuantang.25@gmail.com>
2022-10-20 11:39:52 -0400

    10.20 update

--- assignment-3/python/group script.py
+++ assignment-3/python/group script.py
@@ -1,7 +1,8 @@
 import os
 os.environ["asset"] = "empty group"
 
+import maya.standalone
+maya.standalone.initialize()
+
 import maya.cmds as cmds
 cmds.group(em=True, name=os.environ["asset"])
-
-print("hello")
