ad1a8a86a36f961e28f4a85cbf1610a9ae06a0bd
Tuan Tang <tuantang.25@gmail.com>
2022-10-20 11:24:28 -0400

    testing VS code

--- assignment-3/python/group script.py
+++ assignment-3/python/group script.py
@@ -2,4 +2,6 @@ import os
 os.environ["asset"] = "empty group"
 
 import maya.cmds as cmds
-cmds.group( em=True, name=os.environ["asset"])
+cmds.group(em=True, name=os.environ["asset"])
+
+print("hello")
